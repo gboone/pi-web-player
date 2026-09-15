61d5dad84c2fdfc5702517bf8cd848e87f1e068b
gboone <greg@harmsboone.org>
2026-09-14 19:52:05 -0500

    Transfers the forecast to open meteo for accuracy
    Open Meteo is entirely free and does not require an api key at all it
    also has the benefit of being more accurate and easier to parse. We
    intentionally decided to use standard `requests` for this even though
    open-meteo has it's own API handler. If we were doing something more
    complex that requried pandas or numpy, maybe it would make sense. As it
    stands, though, we really just need tho numbers and WMO code.

--- include/weather.py
+++ include/weather.py
@@ -1,25 +1,69 @@
 from datetime import datetime, date, time
+
+import openmeteo_requests
+from retry_requests import retry
+import requests_cache
 import requests
 
 def convertKtoF(temp):
     tempInK = temp*9/5-459.67
     return round(tempInK,1)
 
-def getTheWeather(token, location, baseurl):
-  # Set up where we'll be fetching data from
-  if len(token) == 0:
-      raise RuntimeError(
-              "You must register a token at https://home.openweathermap.org/users/sign_up then add it to config.yml under `other`, with the name openweather-token."
-      )
-  params = {"q": location, "appid": token}
-  
-  theWeather = requests.get(baseurl, params)
-  theWeatherData = theWeather.json()
-  
-  currentTemp = convertKtoF(theWeather.json()['main']['temp'])
-  highTemp = convertKtoF(theWeather.json()['main']['temp_max'])
-  lowTemp = convertKtoF(theWeather.json()['main']['temp_min'])
-  conditions = theWeather.json()['weather'][0]['main']
-  conditionDescription = theWeather.json()['weather'][0]['description']
-  conditionIcon = theWeather.json()['weather'][0]['icon']
-  return (currentTemp,highTemp,lowTemp,conditions,conditionDescription,conditionIcon)
+def getTheWeather(baseurl, latitude, longitude, params):
+  params['latitude'] = latitude
+  params['longitude'] = longitude
+
+  # Get the forecast from open-meteo. For what we're doing, the openmeteo
+  # api python package is too intense. We just need the values, not to make 
+  # complicated charts or track trends, etc. Just the facts, se we only
+  # use requests.
+  response = requests.get(baseurl, params)
+  # The response contains keys for the different parts of the forecast we asked
+  # the open-meteo API for. Current: current temp and weather code. And Daily:
+  # The high, low, precipitation chance, and sunrise/sunset/daylight values.
+  theWeather = response.json()
+  current = theWeather['current']
+  daily = theWeather['daily']
+  current_temp = current['temperature_2m']
+  current_weather_code = current['weather_code']
+  # Map the above WMO codes to index of icon in 3x3 spritesheet
+  WMO_CODE_TO_ICON = (
+        (0,),  # 0 = sunny
+        (1,),  # 1 = partly sunny/cloudy
+        (2,),  # 2 = cloudy
+        (3,),  # 3 = very cloudy
+        (61, 63, 65),  # 4 = rain
+        (51, 53, 55, 80, 81, 82),  # 5 = showers
+        (95, 96, 99),  # 6 = storms
+        (56, 57, 66, 67, 71, 73, 75, 77, 85, 86),  # 7 = snow
+        (45, 48),  # 8 = fog and stuff
+  )
+  current_weather_icon = next(
+    i for i, t in enumerate(WMO_CODE_TO_ICON) 
+    if current_weather_code in t
+  )
+  # Daily forecast readout:
+  # Same as with Current, the Daily variables appear in the order listed
+  daily = theWeather['daily']
+  highTemp = daily['temperature_2m_max'][0]
+  lowTemp =  daily['temperature_2m_min'][0]
+  precipProbability = daily['precipitation_probability_max'][0]
+  precipHours = daily['precipitation_hours'][0]
+  sunrise = daily['sunrise'][0]
+  sunset = daily['sunset'][0]
+  duration = daily['daylight_duration'][0]
+  return {
+          "current": {
+              "temp": current_temp,
+              "code": current_weather_code,
+              "icon": current_weather_icon
+              },
+          "forecast": {
+              "high": highTemp,
+              "low": lowTemp,
+              "precip": precipProbability,
+              "sunrise": sunrise,
+              "sunset": sunset,
+              "daylight": duration
+              }
+          }
